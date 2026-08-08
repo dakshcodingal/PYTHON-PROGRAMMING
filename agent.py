3adff8f9480e63d80552c5f855cddd83eb565a70
dakshcodingal <hezald28@gmail.com>
2026-08-08 14:38:37 +0400

    agent

---
+++ agent.py
@@ -0,0 +1,7 @@
+name =input("enter your name")
+gadget =input("enter your gadget")
+agentnumber =8
+speedrating =8.9
+print("name ",name," type ",type(name))
+print("gadget ",gadget, "type",type(gadget))
+print(int(gadget))
