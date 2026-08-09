e689253c65a8349c4b6c886c12765c107e105017
dakshcodingal <hezald28@gmail.com>
2026-08-09 14:42:35 +0400

    operators

---
+++ operators.py
@@ -0,0 +1,27 @@
+field1 =120
+field2 =85
+field3 =150
+field4 =95
+field5 =110
+total=field1+field2+field3+field4+field5
+print("total" ,total)
+average=total/5
+print("average" ,average)
+price_per_kg=15
+earning=total*price_per_kg
+print("total earning" ,earning )
+bags=total //25
+leftovergrain=total % 25
+print("full packed bags of 25kg" ,bags)
+print("leftovergrain" ,leftovergrain)
+last_year=500
+print("better than last year? :",total > last_year)
+print("same than last year?",total==last_year)
+total=total + 30
+print("after bonus crop :",total)
+total=total - 15
+print("after seed reverve",total)
+bags=total // 25
+print("total bags filled",bags)
+
+
