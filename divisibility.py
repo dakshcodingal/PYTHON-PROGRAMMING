fa9ab627146bf7141fb0c3ca95f6b7564933570f
dakshcodingal <hezald28@gmail.com>
2026-08-23 14:33:41 +0400

    divisibility.py

---
+++ divisibility.py
@@ -0,0 +1,6 @@
+numerator=int(input("enter numerator"))
+denominator=int(input("enter denominator"))
+if numerator % denominator==0:
+    print("both are divisible")
+else:
+    print("both are not divisible")
