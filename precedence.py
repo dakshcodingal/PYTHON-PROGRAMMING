9968026cb95fa435290236a50714f650f066d758
dakshcodingal <hezald28@gmail.com>
2026-08-23 14:20:10 +0400

    precedence.py

---
+++ precedence.py
@@ -0,0 +1,15 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y
+
+print("value of)(v+w)*x/y : ",z)
+
+name="Alex"
+age=0
+if name=="Alex" or name == "John" and age>2:
+    print("good morning")
+else:
+    print("goodbye")
