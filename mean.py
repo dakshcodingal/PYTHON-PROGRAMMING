e2cf2d3bc5ff58f30b30b37d26797825e364b01c
dakshcodingal <hezald28@gmail.com>
2026-08-23 14:48:08 +0400

    mean.py

---
+++ mean.py
@@ -0,0 +1,9 @@
+total_number=40
+mean=38
+correct_number=56
+wrong_number=36
+sum=mean * total_number
+print("sum of 40 numbers ",sum)
+sum2 = sum-((wrong_number)-(correct_number))
+mean2 = sum2/total_number
+print("final mean",mean2)
